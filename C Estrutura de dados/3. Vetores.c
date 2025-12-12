f25356045d56280bcb2e7b8638c78f74b8ef3241
Levi <levi.farias@aluno.ufca.edu.br>
2025-12-12 10:08:25 -0300

    att

--- C Estrutura de dados/3. Vetores.c
+++ C Estrutura de dados/3. Vetores.c
@@ -3,7 +3,7 @@
 
 int main(){
     #define N 3
-    int i, j, k, x, soma, resto = 0, vetor1[N], vetor2[N], vetor3[N];
+    int i, j, k, x, soma, resto = 0, vetor1[N], vetor2[N], vetor3[N + 1];
     for(int i = 0; i < N; i++){
         printf("Digite um valor de 0 a 9 para adicionar na %i a posicao vetor: ", i + 1);
         scanf("%i", &x);
@@ -35,14 +35,26 @@ int main(){
     }
 
     for(int k = N - 1; k >= 0; k--){
-            soma = vetor1[k] + vetor2[k] + resto
+            soma = vetor1[k] + vetor2[k] + resto;
             if(soma<10){
-                soma = vetor3[k];
+                 vetor3[k + 1] = soma;
                 resto = 0;
             }
             else{
-                vetor3[k] = soma - 10;
+                vetor3[k + 1] = soma - 10;
                 resto = 1;
             }
+    }
+    vetor3[0] = resto;
+    for(int i = 0; i < N; i++){
+        printf("(%i)", vetor1[i]);
+            }
+            printf("\n");
+    for(int i = 0; i < N; i++){
+        printf("(%i)", vetor2[i]);
+            }
+            printf("\n");
+    for(int i = 0; i <= N; i++){
+        printf("(%i)", vetor3[i]);
     }
 }
---
+++ C Estrutura de dados/4. Vetores.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(){
+    #define N 3
+    int i, j, vetor1[N], vetor2[N], vetor3[N * 2];
+    for(int i = 0; i < N; i++){
+        printf("Digite um valor para adicionar na %i a posicao vetor: ", i + 1);
+        scanf("%i", &x);
+    }
+
+    for(int j = 0; j < N; j++){
+        printf("Digite um valor para adicionar na %i a posicao vetor: ", j + 1);
+        scanf("%i", &x);
+
+    }
+}
